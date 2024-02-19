b821c910becf08f83fd8ad2d837c9106e2bd703c
achraf-abartal <134322550+achraf-abartal@users.noreply.github.com>
2024-02-19 16:34:49 +0100

    modif recettecontroller

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -2,18 +2,60 @@
 
 namespace App\Controller;
 
+use App\Entity\Recette;
+use App\Service\Recette\RecetteServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 #[Route('/api/v1/')]
 
 class RecetteController extends AbstractController
 {
-    #[Route('/recette', name: 'app_recette')]
-    public function index(): Response
+    public function __construct(
+        private readonly RecetteServiceInterface $recetteService
+    )
     {
-        return $this->render('recette/index.html.twig', [
-            'controller_name' => 'RecetteController',
-        ]);
+    }
+
+    #[Route('recette', methods: 'POST')]
+    public function addRecette(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent());
+
+        return $this->recetteService->add($data);
+    }
+
+    #[Route('recette/{id}', methods: 'PUT')]
+    public function update(int $id, Request $request): Response
+    {
+        $data = json_decode($request->getContent());
+
+        return $this->recetteService->update($id, $data);
+    }
+
+    #[Route('recette/{id}', methods: 'DELETE')]
+    public function remove(int $id): Response
+    {
+        $this->recetteService->remove($id);
+
+        return new JsonResponse('Supprimé avec succès', Response::HTTP_OK, ['Content-Type' => 'application/json']);
+    }
+
+    #[Route('recette/{id}', methods: 'GET')]
+    public function getOne(int $id): Response
+    {
+        $recette = $this->recetteService->getOne($id);
+
+        return new JsonResponse($recette, Response::HTTP_OK, ['Content-Type' => 'application/json']);
+    }
+
+    #[Route('recettes', methods: 'GET')]
+    public function getAll(): Response
+    {
+        $recettes = $this->recetteService->getAll();
+
+        return new JsonResponse($recettes, Response::HTTP_OK, ['Content-Type' => 'application/json']);
     }
 }
